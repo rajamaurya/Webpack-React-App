6cc5c841afe6e3f8b69217758440968787c2d5f8
Raja krishna Maurya <krishna.raja78@gmail.com>
2023-10-12 13:49:22 +0530

    chore: Added Html Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "src/index.js"),
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "[name].js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Story Teller App",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
 };
